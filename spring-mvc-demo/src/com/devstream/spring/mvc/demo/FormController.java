ec97e4784a0a4d83d99f25c4ee3707240c2ae72b
UdithaIshan <uditha.ishan34@gmail.com>
2021-05-07 20:14:52 +0530

    Creating Controllers and Views

---
+++ spring-mvc-demo/src/com/devstream/spring/mvc/demo/FormController.java
@@ -0,0 +1,31 @@
+package com.devstream.spring.mvc.demo;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class FormController {
+	
+	@RequestMapping("/form")
+	public String showForm() {
+		return "form";
+	}
+	
+	@RequestMapping("/processForm")
+	public String processForm() {
+		return "form_data";
+	}
+
+	@RequestMapping("/processFormWithModel")
+	public String processFormWithModel(HttpServletRequest request, Model model) {
+		String theName = request.getParameter("myname");
+		theName = theName.toUpperCase();
+		String result = "Hello " + theName;
+		model.addAttribute("message", result);
+		return "form_data";
+	}
+}
---
+++ spring-mvc-demo/src/com/devstream/spring/mvc/demo/MainController.java
@@ -0,0 +1,14 @@
+package com.devstream.spring.mvc.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+	
+	@RequestMapping("/")
+	public String showPage() {
+		return "index";
+	}
+
+}
